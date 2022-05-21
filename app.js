f52fd314cb5b13adfb2e8591409944c2a9a6f8e5
rizalabdullah <ra160920@gmail.com>
2022-05-21 16:40:05 +0700

    test

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(`/auth`, authRoute);
 app.use(`/api`, articleRoute);
 app.use(`/api`, categoryRoute);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+//  app.get('*', (req, res) => {
+//    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });   
+}
 
 //Home
 // app.use(`/`, (req, res) => {
@@ -42,12 +48,6 @@ app.use(`/api`, categoryRoute);
 //    })
 //   })
 
-if (process.env.NODE_ENV === 'production') {
-   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-   });   
- }
 
 
 // catch 404 and forward to error handler
